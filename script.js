f8f5d99833ec1a3139420f0be34dd5f73ac229be
Ruslan <die.dice.2003@mail.ru>
2024-07-13 23:39:13 +0500

    Solution

---
+++ script.js
@@ -0,0 +1,10 @@
+﻿const menuActive = document.querySelector(".menu_active");
+const hamburger_menu = document.querySelector(".hamburger_menu");
+const menuClose = document.querySelector(".menu_close");
+
+function toggleMenu() {
+    menuActive.classList.toggle('hidden');
+}
+
+hamburger_menu.addEventListener('click', toggleMenu);
+menuClose.addEventListener('click', toggleMenu);
